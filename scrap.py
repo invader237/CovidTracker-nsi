1d359f184e78a3a8ac19c6909aa54e05df35f307
invader237 <67508521+invader237@users.noreply.github.com>
2022-04-27 22:33:54 +0200

    :building_construction:(scrap.py): put the code in a function

--- scrap.py
+++ scrap.py
@@ -3,11 +3,12 @@
 
 import requests
 
-#Makes a "get" request to the specified url with a maximum delay of 30ms
-r = requests.get('https://www.data.gouv.fr/fr/datasets/r/8e5e70fa-c082-45e3-a7b8-20862711b142', timeout=30, allow_redirects=True)
+def csv_download():
+    #Makes a "get" request to the specified url with a maximum delay of 30ms
+    r = requests.get('https://www.data.gouv.fr/fr/datasets/r/8e5e70fa-c082-45e3-a7b8-20862711b142', timeout=30, allow_redirects=True)
 
-#Writes the query response to a file
-open("vacsi-s-a-reg-2022-03-22-19h01.csv", "wb").write(r.content)
+    #Writes the query response to a file
+    open("vacsi-s-a-reg-2022-03-22-21h01.csv", "wb").write(r.content)
 
 
 
